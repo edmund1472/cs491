94f264c4ab6aabe2194946b50c2efd1c03d5f9aa
edmund1472 <edmundzhong2@gmail.com>
2026-03-04 15:30:21 -0500

    testing 2

---
+++ houseandroom/houseandroomhw/src/components/House.jsx
@@ -0,0 +1,15 @@
+
+
+
+
+import Room from "./Room";
+
+export default function House(props) {
+  return (
+    <div>
+      My House
+      <h2>House: {props.name}</h2>
+      <Room name="bedroom" houseName={props.name}></Room>
+    </div>
+  );
+}
